56ad64c7ddfca63f58bbf1d2a7c94bffe9ad9311
jasmin <chjasmin27@gmail.com>
2024-05-26 11:36:02 +0600

    ..

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // );
 
 app.use(cors({
-  origin: '*', // Allow all origins
+  origin: 'http://localhost:5173', // Allow all origins
   credentials: true, // Allow credentials
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
