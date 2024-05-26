b70a911b36ce2f29e0486876904fac6b3d28cb18
jasmin <chjasmin27@gmail.com>
2024-05-26 11:29:32 +0600

    ....

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials : true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials : true,
+    origin: (origin, callback) => {
+      // Allow requests with no origin like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      callback(null, origin);
+    },
+    credentials: true, // Allow credentials
   })
 );
 
